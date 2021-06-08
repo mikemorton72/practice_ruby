f2db1400b44b3f11d3da9f2d12cb3f05d544293a
mike <mmorton7295@gmail.com>
2021-06-07 21:11:44 -0500

    add Kata04 exercise folder

---
+++ Kata04/weather_data.rb
@@ -0,0 +1,11 @@
+#read weather.dat and import into script
+weather_data = File.open("weather.dat", "r")
+#parse data of weather.dat to extract maximum and minimum temp of each day. Hash structure.
+daily_temps = {}
+p weather_data.lines
+
+#calculate temperature spread for each day and add to hash
+
+#find the smallest temperature spread using a for loop
+
+#output day number
