12005d0bf3ef80181cf1de3c542cbbcca851d936
mike <mmorton7295@gmail.com>
2021-06-20 19:59:41 -0500

    completes weather_data.rb

--- Kata04/weather_data.rb
+++ Kata04/weather_data.rb
@@ -1,11 +1,17 @@
 #read weather.dat and import into script
-weather_data = File.open("weather.dat", "r")
-#parse data of weather.dat to extract maximum and minimum temp of each day. Hash structure.
-daily_temps = {}
-p weather_data.lines
+weather_data = File.open("weather.dat", "r").readlines
 
-#calculate temperature spread for each day and add to hash
+#parse data of weather.dat to extract a temp difference for each day. Array of hashes structure.
+daily_temps = []
+weather_data.map! {|line| line.split(" ")}
+weather_data.each do |line|
+  unless line[0] == "Dy" || line[0] == nil || line[0] == "mo" #ignore header, monthly sum, blank line
+    daily_temps << {day: line[0], temp_spread: (line[1].to_i - line[2].to_i)}
+  end
+end
 
-#find the smallest temperature spread using a for loop
+daily_temps.sort_by! {|day| day[:temp_spread]}
+lowest_spread = daily_temps[0]
 
 #output day number
+puts "Day #{lowest_spread[:day]} has the lowest temperature spread of #{lowest_spread[:temp_spread]} degrees. "
